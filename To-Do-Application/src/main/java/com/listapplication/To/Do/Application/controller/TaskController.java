f3ba1938f09ab854e8982c1a87b94659ce8ed518
Nonhlanhla25 <nonhlanhlamthimkulu@gmail.com>
2025-04-01 21:49:01 +0200

    Added a PostMapping and form in html

--- To-Do-Application/src/main/java/com/listapplication/To/Do/Application/controller/TaskController.java
+++ To-Do-Application/src/main/java/com/listapplication/To/Do/Application/controller/TaskController.java
@@ -5,8 +5,12 @@ import com.listapplication.To.Do.Application.service.TaskService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+
+import static jdk.javadoc.internal.doclets.formats.html.markup.HtmlStyle.title;
 
 
 @Controller
@@ -17,11 +21,18 @@ public class TaskController {
         this.taskService = taskService;
     }
 
-    @GetMapping
-    public String getTask(Model model){
+    //handler method to handle tasks
+    @GetMapping("/tasks")
+    public String getTasks(Model model){
         List<Task> tasks = taskService.getAllTasks();
-        model.addAttribute("task",tasks);
-        return "tasks.html";
+        model.addAttribute("tasks",tasks);
+        return "tasks";
+    }
+
+    @PostMapping("")
+    public String createTask(RequestParam String title){
+        taskService.createTask(java.lang.String.valueOf(title));
+        return "redirect:/";
     }
 
 
