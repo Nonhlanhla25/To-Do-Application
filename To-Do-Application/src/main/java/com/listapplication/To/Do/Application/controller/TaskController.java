53c907f0a28d461c1428a0166fe39d158fad9d05
Nonhlanhla25 <nonhlanhlamthimkulu@gmail.com>
2025-04-05 13:18:33 +0200

    Added the delete task function and status function

--- To-Do-Application/src/main/java/com/listapplication/To/Do/Application/controller/TaskController.java
+++ To-Do-Application/src/main/java/com/listapplication/To/Do/Application/controller/TaskController.java
@@ -4,13 +4,9 @@ import com.listapplication.To.Do.Application.models.Task;
 import com.listapplication.To.Do.Application.service.TaskService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
-import static jdk.javadoc.internal.doclets.formats.html.markup.HtmlStyle.title;
 
 
 @Controller
@@ -22,18 +18,37 @@ public class TaskController {
     }
 
     //handler method to handle tasks
-    @GetMapping("/tasks")
+    @GetMapping("/")
     public String getTasks(Model model){
         List<Task> tasks = taskService.getAllTasks();
         model.addAttribute("tasks",tasks);
         return "tasks";
     }
 
-    @PostMapping("")
-    public String createTask(RequestParam String title){
-        taskService.createTask(java.lang.String.valueOf(title));
+    //post method to post tasks
+    @PostMapping("/")
+    public String createTask(@RequestParam String title){
+        taskService.createTask(title);
         return "redirect:/";
     }
+
+    //delete the task by id
+    @GetMapping("/{id}/delete")
+    public String deleteTask(@PathVariable Long id){
+        taskService.deleteTask(id);
+        return "redirect:/";
+    }
+
+    //handles the status of the task by id if it's completed or not completed
+    @GetMapping("/{id}/status")
+    public String statusTask(@PathVariable Long id){
+        taskService.statusTask(id);
+        return "redirect:/";
+    }
+
+
+
+
 
 
 
--- To-Do-Application/src/main/java/com/listapplication/To/Do/Application/service/TaskService.java
+++ To-Do-Application/src/main/java/com/listapplication/To/Do/Application/service/TaskService.java
@@ -26,4 +26,15 @@ public class TaskService {
         task.setCompleted(false);
         taskRepository.save(task);
     }
+
+    public void deleteTask(Long id) {
+        taskRepository.deleteById(id);
+    }
+
+    public void statusTask(Long id) {
+        Task task = taskRepository.findById(id)
+                .orElseThrow(()-> new IllegalArgumentException("Invalid task id"));
+                task.setCompleted(!task.isCompleted());
+                taskRepository.save(task);
+    }
 }
